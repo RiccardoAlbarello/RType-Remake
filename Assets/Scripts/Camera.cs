54c52e23d673613b904ae943e6785d5716a36cea
RiccardoAlbarello <albarello.riccardo@gmail.com>
2023-02-08 23:23:30 +0100

    Movimento camera migliorata e aggiunto movimento giocatore iniziale

--- Assets/Scripts/Camera.cs
+++ Assets/Scripts/Camera.cs
@@ -4,18 +4,56 @@ using UnityEngine;
 
 public class Camera : MonoBehaviour
 {
+    [SerializeField] List<GameObject> points;
 
-    [SerializeField] float cameraSpeed = 5;
+    [SerializeField] float cameraSpeed;
+
+    [SerializeField] int index = 0;
+
+    bool cameraStop = true;
     
+   
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        cameraStop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * cameraSpeed * Time.deltaTime);
+
+        MovementCamera();
     }
+
+    void MovementCamera()
+    {
+        transform.position = Vector3.MoveTowards(transform.position, points[index].transform.position, cameraSpeed * Time.deltaTime);
+
+        if (Vector3.Distance(transform.position, points[index].transform.position) < 1 && cameraStop == true )
+        {
+            index++;
+
+            
+
+        }
+
+        if(index == 1)
+        {
+            
+            cameraStop = false;
+
+        }
+
+
+          
+
+        
+        
+        
+
+    }
+
+
 }
--- Assets/Scripts/MovementPlayer.cs
+++ Assets/Scripts/MovementPlayer.cs
@@ -6,17 +6,35 @@ public class MovementPlayer : MonoBehaviour
 {
 
     [SerializeField] float playerSpeed = 5f;
+
     
+    
+    
+
+    [SerializeField] List<GameObject> points;
+
+    [SerializeField] float automaticSpeed;
+    
+
+    [SerializeField] int index = 0;
+
+    bool playerStop = true;
+
+    [SerializeField] GameObject objecttospawnstart;
+
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerStop = true;
     }
 
     // Update is called once per frame
     void Update()
     {
         PlayerMovement();
+        MovementPlayerAuto();
     }
 
 
@@ -28,6 +46,37 @@ public class MovementPlayer : MonoBehaviour
         Vector3 direction = new Vector3(HorizontalInput, VerticalInput, 0);
         transform.Translate(direction * playerSpeed * Time.deltaTime);
     }
+
+    void MovementPlayerAuto()
+    {
+            
+            if(index == 1)
+        {
+         transform.position = Vector3.MoveTowards(transform.position, points[index].transform.position, automaticSpeed * Time.deltaTime);
+
+        }
+
+            
+        
+
+        if (Vector3.Distance(transform.position, points[index].transform.position) == 0 && playerStop == true)
+        {
+            index++;
+            
+            
+            
+           
+        }
+
+        if (index == 2)
+        {
+            index = 0;
+            Instantiate(objecttospawnstart);
+            
+
+        }
+    }
+
 
 
 
